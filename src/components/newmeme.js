95398a995242a9096061cb5856b97919398745cf
igar1991 <iiggaarr19912004@gmail.com>
2022-04-11 15:32:45 +0300

    feat: new meme tenplate

--- src/components/newmeme.js
+++ src/components/newmeme.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {  getTemplateArr } from '../redux/slices/mainSlice';
+import template_meme from './all-memez.json'
 
 
 export const Newmeme = ({ closeM }) => {
     const dispatch = useDispatch()
 
-    const { imgTemplate } = useSelector(state => state.main)
+    console.log(template_meme)
+
+    const imgTemplate = template_meme
 
     useEffect(() => {
         dispatch(getTemplateArr())
@@ -17,10 +20,7 @@ export const Newmeme = ({ closeM }) => {
             {imgTemplate?.map((image, index) => {
                 return (
                     <button key={index} className="bg-gray card m-2 shadow p-0" style={{ width: "10rem" }} onClick={() => closeM(image)}>
-                        <img src={image.url} style={{ height: "9rem", objectFit: "cover" }} className="card-img-top" alt="meme" />
-                        <div className="card-body">
-                            <p className="card-text fw-bold">{image.name}</p>
-                        </div>
+                        <img src={image.path} style={{ height: "9rem", objectFit: "cover" }} className="card-img-top" alt="meme" />
                     </button>
                 )
             })}
